79781bb3d120753c0f9af0dafc19f4145f09ff9e
Subham Adhikari <subhamadhikari20@gmail.com>
2023-11-21 14:05:00 +0545

    Adding test file

---
+++ test.py
@@ -0,0 +1,10 @@
+import random
+
+random_number = random.randint(0,2)
+print(random_number)
+print(".............Snake Gun Water Game...............")
+a = int(input("Enter 0 for snake, 1 for water: "))
+
+if (random_number == a) :
+    print("")
+
