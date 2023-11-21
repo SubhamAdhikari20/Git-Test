9eee9f0ebd299ec3849ef20b128346054aa3f502
Subham Adhikari <subhamadhikari20@gmail.com>
2023-11-21 14:23:58 +0545

    Snake, Water and Gun Game

--- test.py
+++ test.py
@@ -1,10 +1,30 @@
 import random
 
-random_number = random.randint(0,2)
-print(random_number)
+comp = random.randint(0,2)
+# print(comp)
 print(".............Snake Gun Water Game...............")
-a = int(input("Enter 0 for snake, 1 for water: "))
+user = int(input("Enter 0 for snake, 1 for water and 2 for gun: "))
 
-if (random_number == a) :
-    print("")
+def test(comp, user):
+    if (comp == user):
+        return 0
+    if (comp == 0 and user == 1):
+        return -1
+    if (comp == 1 and user == 2):
+        return -1
+    if (comp == 2 and user == 0):
+        return-1
+    return 1
+
+score = test(comp, user)
+
+print("You: ", user)
+print("Computer: ", comp)
+
+if(score == 0):
+  print("Its a draw")
+elif (score == -1):
+  print("You Lose")
+else:
+  print("You Won")
 
